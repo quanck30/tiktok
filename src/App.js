87024e0e069c38c7e663add8bf748c8d76f1b0fd
quanck30 <quanck30@gmail.com>
2022-09-28 10:18:01 +0700

    #

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes'
+import { DefaultLayout } from './components/Layouts'
 function App() {
   return (
-    <div className="App">
-      <h1>React JS</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component
+
+            let Layout = DefaultLayout
+            if (route.layout === null) {
+              Layout = Fragment
+            } else if (route.layout) {
+              Layout = route.layout
+            }
+            return <Route key={index} path={route.path} element={
+              <Layout>
+                <Page />
+              </Layout>
+            }
+            />
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
